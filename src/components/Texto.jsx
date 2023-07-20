38d7baa3775c519c09ed4a73f84a2cb2807d9366
Nico Almiron <almiron.nicolas16@gmail.com>
2023-07-20 15:38:49 -0300

    se consluyo la funcion del ejercicio

---
+++ src/components/Texto.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Texto = ({ textoModificado }) => {
+  return (
+    <section>
+      <h1>{textoModificado}</h1>
+    </section>
+  );
+};
+
+export default Texto;
